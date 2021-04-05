00a307fd5d996ce9b190544a865a954cd455a6fb
Samarone <samarone@alunos.utfpr.edu.br>
2021-04-05 18:34:11 -0300

    05-04-18:

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Cliente - Samarone</title>
+        <link rel="stylesheet" type="text/css" href="/css/my.css" />
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
+        
+    </head>
+    <body class="body-login">
+        <div id="login">
+            <h3 class="text-center text-white pt-5">Primeira Tela</h3>
+        </div>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Cliente - Samarone</title>
-        <!--link rel="stylesheet" type="text/css" href="{{asset('css/my.css')}}"-->
         <link rel="stylesheet" type="text/css" href="/css/my.css" />
-        
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
         
     </head>
     <body class="body-login">
         <div id="login">
-            <h3 class="text-center text-white pt-5">Login form</h3>
+            <h3 class="text-center text-white pt-5">Sistema de Solicitações</h3>
             <div class="container">
                 <div id="login-row" class="row justify-content-center align-items-center">
                     <div id="login-column" class="col-md-6">
@@ -20,19 +19,18 @@
                             <form id="login-form" class="form" action="" method="post">
                                 <h3 class="text-center text-info">Login</h3>
                                 <div class="form-group">
-                                    <label for="username" class="text-info">Username:</label><br>
+                                    <label for="username" class="text-info">Usuario:</label><br>
                                     <input type="text" name="username" id="username" class="form-control">
                                 </div>
                                 <div class="form-group">
-                                    <label for="password" class="text-info">Password:</label><br>
+                                    <label for="password" class="text-info">Senha:</label><br>
                                     <input type="text" name="password" id="password" class="form-control">
                                 </div>
-                                <div class="form-group">
-                                    <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                    <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                                <div class="form-group m-4 text-center">
+                                    <input type="submit" name="submit" class="btn btn-info btn-md" value="Entrar">
                                 </div>
                                 <div id="register-link" class="text-right">
-                                    <a href="#" class="text-info">Register here</a>
+                                    <!--<a href="{{ route('inicio') }}" class="text-info">Cadastrar-se</a>-->
                                 </div>
                             </form>
                         </div>
